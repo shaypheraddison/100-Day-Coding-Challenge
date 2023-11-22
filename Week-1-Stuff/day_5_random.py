caeeb619cd69adc6ab7dc6ddfc44dcd9027a9e7f
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-22 15:57:44 -0600

    Update day_5_random.py

--- Week-1-Stuff/day_5_random.py
+++ Week-1-Stuff/day_5_random.py
@@ -10,8 +10,16 @@ print(random_decimal)
 print(random_float)
 
 #random coin flip to show heads or tails
+#import random
 random_coin = random.randint(0,1)
 if random_coin == 1:
   print("Heads")
 else:
   print("Tails")
+
+#select a random name from a list practice
+#import random
+names = ["Greg", "Shaun", "Liam", "Kara", "Liz", "Dave"]
+names.append("Spencer")
+random_name = random.choice(names)
+print(f"{random_name} is going to pay for the meal today!")
