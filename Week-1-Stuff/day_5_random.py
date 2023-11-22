d6d14e10cd12192abdad4127ec812d429f599e52
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-22 11:22:27 -0600

    Create day_5_random.py

---
+++ Week-1-Stuff/day_5_random.py
@@ -0,0 +1,10 @@
+import random
+#random whole numbers
+random_integer = random.randint(1, 50)
+print(random_integer)
+
+#random float numbers
+random_decimal = random.random()
+random_float = random.random() * 5
+print(random_decimal)
+print(random_float)
