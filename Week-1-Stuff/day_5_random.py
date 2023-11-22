7b88d50f19ba7f750ddbdd4e2f6b2b4f3431978b
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-22 11:29:41 -0600

    Update day_5_random.py

--- Week-1-Stuff/day_5_random.py
+++ Week-1-Stuff/day_5_random.py
@@ -8,3 +8,10 @@ random_decimal = random.random()
 random_float = random.random() * 5
 print(random_decimal)
 print(random_float)
+
+#random coin flip to show heads or tails
+random_coin = random.randint(0,1)
+if random_coin == 1:
+  print("Heads")
+else:
+  print("Tails")
