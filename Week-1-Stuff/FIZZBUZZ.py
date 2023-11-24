76fc7319ddd859787b598c06d640f5366834ce9d
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-24 01:07:36 -0600

    Create FIZZBUZZ.py

---
+++ Week-1-Stuff/FIZZBUZZ.py
@@ -0,0 +1,11 @@
+#Lesson was to create the FIZZBUZZ game with code...potential question asked when hiring to produce this code !!!
+
+for number in range(1, 101):
+  if number %3 == 0 and number %5 == 0:
+    print("FizzBuzz")
+  elif number %3 ==0:
+    print("Fizz")
+  elif number %5 == 0:
+    print("Buzz")
+  else:
+    print(number)
