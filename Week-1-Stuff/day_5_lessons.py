4bd41c26814b460bf2a8096ebf0dc36d8c09c46b
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-23 23:56:09 -0600

    Create day_5_lessons.py

---
+++ Week-1-Stuff/day_5_lessons.py
@@ -0,0 +1,19 @@
+# Using ONLY FOR LOOPS to solve getting the avg height(in cm), total number of students in the input created list and to sum up the total height from the list
+
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+sum_height = 0
+for height in student_heights:
+  sum_height += height
+
+total_students = 0
+for total in student_heights:
+  total_students += 1
+
+avg_height = int(round(sum_height / total_students))
+  
+print(f"total height = {sum_height}")
+print(f"number of students = {total_students}")
+print(f"average height = {avg_height}")
