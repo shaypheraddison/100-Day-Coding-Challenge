8ad997cc16b5c157460bbdb6e34cb09ecd25bd77
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-24 00:17:50 -0600

    Update day_5_lessons.py

--- Week-1-Stuff/day_5_lessons.py
+++ Week-1-Stuff/day_5_lessons.py
@@ -17,3 +17,15 @@ avg_height = int(round(sum_height / total_students))
 print(f"total height = {sum_height}")
 print(f"number of students = {total_students}")
 print(f"average height = {avg_height}")
+
+#Using ONLY FOR LOOPS to solve locating the highest number in a input list 
+
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+  
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+print(f"The highest score in the class is: {highest_score}")
