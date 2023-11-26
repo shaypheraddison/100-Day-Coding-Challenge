a9a6981c1d2b9f75f4191eab197e933ac6952d0d
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-11-26 01:21:26 -0600

    Create hangman_game.py

---
+++ Week-1-Stuff/hangman_game.py
@@ -0,0 +1,43 @@
+#Day 7 project to make a hangman game
+import os
+clear = lambda: os.system('cls')
+import random
+import hangman_art as stages
+import hangman_words as word_list
+
+chosen_word = random.choice(word_list.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(stages.logo)
+
+# #Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+      print(f"You have already guessed {guess}.")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        print(f"{guess} is not in the chosen word, please guess again.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages.stages[lives])
